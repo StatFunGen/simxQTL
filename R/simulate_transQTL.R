8285398e6ac125994e83106dab97ae1082a9d869
xuewei cao <36172337+xueweic@users.noreply.github.com>
2023-12-20 08:22:44 -0500

    Update simulate_transQTL.R

--- R/simulate_transQTL.R
+++ R/simulate_transQTL.R
@@ -21,159 +21,130 @@
 #'
 #' @export
 simulate_cis_expression <- function(G, A, phi_v) {
-  n <- nrow(G)
-  m <- ncol(A)
-  E <- matrix(NA, n, m)
-
-  for (j in 1:m) {
-    connected_snps <- which(A[, j] == 1)
-    num_snps <- length(connected_snps)
-
-    beta <- rep(0, ncol(G))
-    for (i in connected_snps) {
-      if (i == connected_snps[1]) {
-        beta[i] <- 1
-      } else {
-        beta[i] <- sqrt(beta[connected_snps[1]]^2 * var(G[, connected_snps[1]]) / var(G[, i]))
-      }
-    }
-
-    # Corrected calculation of variance_sum and sigma2_cis
-    variance_sum <- var(G[, connected_snps, drop = FALSE] %*% beta[connected_snps])
-    sigma2_cis <- var(G[, connected_snps[1]] * beta[connected_snps[1]]) / phi_v - variance_sum
-    while (sigma2_cis <= 0) {
-      phi_v <- phi_v - 0.01
-      sigma2_cis <- var(E_comb[, affecting_genes[1]] * beta[affecting_genes[1]]) / phi_gene - variance_sum
-    }
-
-    # Simulate gene expression
-    E_tmp <- G %*% beta + rnorm(n, mean = 0, sd = sqrt(sigma2_cis))
-    E[, j] <- scale(E_tmp)
-  }
-
-  return(E)
-}
-
-
-#' Simulate trans gene expression (Type I error simulations)
-#'
-#' This function simulates trans gene expression based on an adjacency matrix and per-gene heritability.
-#' It generates an expression matrix E with standardized normal distribution for some genes,
-#' and a multi-regression model for others.
-#'
-#' @param A The adjacency matrix of gene-gene regulatory causal relationship with dimensions g x g.
-#' @param phi_gene The per-gene heritability value.
-#' @param n The sample size.
-#'
-#' @return A matrix E of dimensions n x g representing the simulated gene expression.
-#'
-#' @examples
-#' g <- 9
-#' n <- 1000
-#' A <- matrix(0, nrow = g, ncol = g)
-#' A[5:9, 5:9] <- matrix(sample(0:1, 5 * 5, replace = TRUE), nrow = 5)
-#' phi_gene <- 0.15
-#' E <- simulation_trans_expression_t1e(A, phi_gene, n)
-#'
-#' @export
-simulate_trans_expression_t1e <- function(A, phi_gene, n) {
-  g <- ncol(A)
-  E <- matrix(rnorm(n * g), n, g)  # Initialize E with standard normal distribution
-
-  for (j in 1:g) {
-    affecting_genes <- which(A[, j] == 1)
-    beta <- rep(0, g)  # Initialize beta to zero
-
-    if (length(affecting_genes) > 0) {
-      beta[affecting_genes[1]] <- 1  # Set beta to 1 for the first affecting gene
-      
-      if (length(affecting_genes) > 1) {
-        for (i in affecting_genes[-1]) {
-          beta[i] <- sqrt(var(E[, affecting_genes[1]]) / var(E[, i]))
+    n <- nrow(G)
+    m <- ncol(A)
+    E <- matrix(NA, n, m)
+    
+    for (j in 1:m) {
+        connected_snps <- which(A[, j] == 1)
+        num_snps <- length(connected_snps)
+        
+        beta <- rep(0, ncol(G))
+        for (i in connected_snps) {
+            if (i == connected_snps[1]) {
+                beta[i] <- 1
+            } else {
+                beta[i] <- sqrt(beta[connected_snps[1]]^2 * var(G[, connected_snps[1]]) / var(G[, i]))
+            }
         }
-      }
-
-      # Calculate sigma^2_trans
-      variance_sum <- var(E[, affecting_genes, drop = FALSE] %*% beta[affecting_genes])
-      sigma2_trans <- var(E[, affecting_genes[1]] * beta[affecting_genes[1]]) / phi_gene - variance_sum
-      while (sigma2_trans <= 0) {
-        phi_gene <- phi_gene - 0.01
-        sigma2_trans <- var(E_comb[, affecting_genes[1]] * beta[affecting_genes[1]]) / phi_gene - variance_sum
-      }
-
-      # Multi-regression model for gene expression
-      E_tmp <- E %*% beta + rnorm(n, mean = 0, sd = sqrt(sigma2_trans))
-      E[, j] <- scale(E_tmp)
+        
+        # Corrected calculation of variance_sum and sigma2_cis
+        variance_sum <- var(G[, connected_snps, drop = FALSE] %*% beta[connected_snps])
+        sigma2_cis <- var(G[, connected_snps[1]] * beta[connected_snps[1]]) / phi_v - variance_sum
+        while (sigma2_cis <= 0) {
+            phi_v <- phi_v - 0.01
+            sigma2_cis <- var(E_comb[, affecting_genes[1]] * beta[affecting_genes[1]]) / phi_gene - variance_sum
+        }
+        
+        # Simulate gene expression
+        E_tmp <- G %*% beta + rnorm(n, mean = 0, sd = sqrt(sigma2_cis))
+        E[, j] <- scale(E_tmp)
     }
-  }
-
-  return(E)
+    
+    return(E)
 }
 
-#' Simulate trans gene expression for accuracy and false discovery rate simulations
+
+
+#' Simulate trans gene expression for different simulation scenarios
 #'
-#' This function simulates trans gene expression based on cis and trans gene expression data and adjacency matrices.
-#' It creates a combined expression matrix and adjacency matrix for simulation.
+#' This function can perform Type I error simulations or Accuracy and False Discovery Rate (ACC/FDR) simulations
+#' depending on the specified type. For T1E simulations, it only needs an adjacency matrix and heritability. 
+#' For ACC/FDR simulations, it requires additional cis gene expression and cis-trans adjacency matrix.
 #'
-#' @param E_cis Matrix of cis gene expressions with dimensions n x m.
-#' @param A_cis_trans Adjacency matrix between cis and trans genes with dimensions m x g.
-#' @param A_trans Adjacency matrix among trans genes with dimensions g x g.
-#' @param phi_gene Per-gene heritability.
-#'
-#' @return A matrix E_trans of dimensions n x g representing the simulated trans gene expression.
+#' @param A_trans Adjacency matrix among trans genes with dimensions g x g, required for both T1E and ACC/FDR.
+#' @param phi_gene Per-gene heritability, required for both T1E and ACC/FDR.
+#' @param n Sample size, required only for T1E simulations.
+#' @param E_cis Matrix of cis gene expressions with dimensions n x m, required only for ACC/FDR simulations.
+#' @param A_cis_trans Adjacency matrix between cis and trans genes with dimensions m x g, required for ACC/FDR simulations.
+#' @param type Type of simulation to perform, either 'T1E' or 'ACC'.
+#' @return A matrix E representing the simulated gene expression.
 #'
 #' @examples
-#' n <- 1000
-#' m <- 8
+#' # For T1E simulation
 #' g <- 9
+#' n <- 1000
+#' A_trans <- matrix(sample(0:1, g * g, replace = TRUE), nrow = g)
+#' phi_gene <- 0.15
+#' E <- simulate_trans_expression(A_trans, phi_gene, n = n, type = 'T1E')
+#'
+#' # For ACC/FDR simulation
+#' m <- 8
 #' E_cis <- matrix(rnorm(n * m), n, m)
 #' A_cis_trans <- matrix(sample(0:1, m * g, replace = TRUE), nrow = m)
-#' A_trans <- matrix(sample(0:1, g * g, replace = TRUE), nrow = g)
-#' phi_gene <- 0.20
-#' E_trans <- simulation_trans_expression_ACC_FDR(E_cis, A_cis_trans, A_trans, phi_gene)
-#'
+#' E <- simulate_trans_expression(A_trans, phi_gene, E_cis = E_cis, A_cis_trans = A_cis_trans, type = 'ACC')
 #' @export
-simulate_trans_expression_ACC_FDR <- function(E_cis, A_cis_trans, A_trans, phi_gene) {
-  n <- nrow(E_cis)
-  m <- ncol(E_cis)
-  g <- ncol(A_trans)
-  E_trans <- matrix(0, n, g)  # Initialize E_trans with zeros
-  E_comb <- cbind(E_cis, E_trans)  # Combine E_cis and E_trans
-
-  # Create combined adjacency matrix A_comb
-  A_comb <- rbind(A_cis_trans, A_trans)
-
-  for (j in 1:g) {
-    affecting_genes <- which(A_comb[, j] == 1)
-    beta <- rep(0, m + g)  # Initialize beta to zero
-
-    if (length(affecting_genes) > 0) {
-      beta[affecting_genes[1]] <- 1  # Set beta to 1 for the first affecting gene
-      
-      if (length(affecting_genes) > 1) {
-        for (i in affecting_genes[-1]) {
-          beta[i] <- sqrt(var(E_comb[, affecting_genes[1]]) / var(E_comb[, i]))
-        }
-      }
-
-      # Calculate sigma^2
-      variance_sum <- var(E_comb[, affecting_genes, drop = FALSE] %*% beta[affecting_genes])
-      sigma2_gene <- var(E_comb[, affecting_genes[1]] * beta[affecting_genes[1]]) / phi_gene - variance_sum
-      while (sigma2_gene <= 0) {
-        phi_gene <- phi_gene - 0.01
-        sigma2_gene <- var(E_comb[, affecting_genes[1]] * beta[affecting_genes[1]]) / phi_gene - variance_sum
-      }
-
-      # Multi-regression model for gene expression
-      E_tmp <- E_comb %*% beta + rnorm(n, mean = 0, sd = sqrt(sigma2_gene))
-      E_comb[, m+j] <- scale(E_tmp)
+simulate_trans_expression <- function(A_trans, phi_gene, n = NULL, E_cis = NULL, A_cis_trans = NULL, type = 'T1E') {
+    if (type == 'T1E' && is.null(n)) {
+        stop("For T1E simulation, 'n' must be provided.")
     }
-  }
-
-  # Extract the trans gene expression part from the combined matrix
-  E_trans <- E_comb[, (m + 1):(m + g)]
-  return(E_trans)
+    if (type == 'ACC' && (is.null(E_cis) || is.null(A_cis_trans))) {
+        stop("For ACC/FDR simulation, 'E_cis' and 'A_cis_trans' must be provided.")
+    }
+    
+    if (type == 'ACC') n <- nrow(E_cis)
+    g <- ncol(A_trans)
+    E <- if (type == 'T1E') matrix(rnorm(n * g), n, g) else matrix(0, n, g)
+    
+    if (type == 'T1E') {
+        for (j in 1:g) {
+            affecting_genes <- which(A_trans[, j] == 1)
+            beta <- rep(0, g)
+            if (length(affecting_genes) > 0) {
+                beta[affecting_genes[1]] <- 1
+                for (i in affecting_genes[-1]) {
+                    beta[i] <- sqrt(var(E[, affecting_genes[1]]) / var(E[, i]))
+                }
+                variance_sum <- var(E[, affecting_genes, drop = FALSE] %*% beta[affecting_genes])
+                sigma2_trans <- var(E[, affecting_genes[1]] * beta[affecting_genes[1]]) / phi_gene - variance_sum
+                while (sigma2_trans <= 0) {
+                    phi_gene <- phi_gene - 0.01
+                    sigma2_trans <- var(E[, affecting_genes[1]] * beta[affecting_genes[1]]) / phi_gene - variance_sum
+                }
+                # Simulate gene expression
+                E_tmp <- E %*% beta + rnorm(n, mean = 0, sd = sqrt(sigma2_trans))
+                E[, j] <- scale(E_tmp)
+            }
+        }
+    } else if (type == 'ACC') {
+        m <- ncol(E_cis)
+        E_comb <- cbind(E_cis, E)
+        A_comb <- rbind(A_cis_trans, A_trans)
+        for (j in 1:g) {
+            affecting_genes <- which(A_comb[, j] == 1)
+            beta <- rep(0, m + g)
+            if (length(affecting_genes) > 0) {
+                beta[affecting_genes[1]] <- 1
+                for (i in affecting_genes[-1]) {
+                    beta[i] <- sqrt(var(E_comb[, affecting_genes[1]]) / var(E_comb[, i]))
+                }
+                variance_sum <- var(E_comb[, affecting_genes, drop = FALSE] %*% beta[affecting_genes])
+                sigma2_gene <- var(E_comb[, affecting_genes[1]] * beta[affecting_genes[1]]) / phi_gene - variance_sum
+                while (sigma2_gene <= 0) {
+                    phi_gene <- phi_gene - 0.01
+                    sigma2_gene <- var(E_comb[, affecting_genes[1]] * beta[affecting_genes[1]]) / phi_gene - variance_sum
+                }
+                # Multi-regression model for gene expression
+                E_tmp <- E_comb %*% beta + rnorm(n, mean = 0, sd = sqrt(sigma2_gene))
+                E_comb[, m+j] <- scale(E_tmp)
+            }
+        }
+        E <- E_comb[, (m + 1):(m + g)]
+    }
+    return(E)
 }
+
+
 
 #' Simulate trans gene expression for a mixture of cell types
 #'
@@ -199,25 +170,28 @@ simulate_trans_expression_ACC_FDR <- function(E_cis, A_cis_trans, A_trans, phi_g
 simulate_trans_mixture_celltype <- function(E_cis, phi_gene, A_cell_1, 
                                             A_cell_2 = NULL, omega = NULL, 
                                             p = c(1/3, 2/3)) {
-
-  if (is.null(A_cell_2)) {
-    # Generate random A for cell type 2 if A_cell_2 is not provided
-    A_cell_2 <- get_random_A(A_cell_1, p)
-  }
-
-  # Simulate for cell type 1 and 2
-  E_trans_cell_1 <- simulate_trans_expression_ACC_FDR(E_cis, A_cell_1$A_cis_trans, A_cell_1$A_trans, phi_gene)
-  E_trans_cell_2 <- simulate_trans_expression_ACC_FDR(E_cis, A_cell_2$A_cis_trans, A_cell_2$A_trans, phi_gene)
-
-  # Determine omega if not provided
-  if (is.null(omega)) {
-    omega <- runif(1, 0.2, 0.8)
-  }
-
-  # Weighted sum
-  E_trans <- omega * E_trans_cell_1 + (1 - omega) * E_trans_cell_2
-
-  return(list(E_trans = E_trans, E_trans_cell_1 = E_trans_cell_1, E_trans_cell_2 = E_trans_cell_2))
+    
+    if (is.null(A_cell_2)) {
+        # Generate random A for cell type 2 if A_cell_2 is not provided
+        A_cell_2 <- get_random_A(A_cell_1, p)
+    }
+    
+    
+    # Simulate for cell type 1 and 2
+    E_trans_cell_1 <- simulate_trans_expression(A_trans = A_cell_1$A_trans, phi_gene = phi_gene,
+                                                E_cis = E_cis,A_cis_trans = A_cell_1$A_cis_trans, type = 'ACC')
+    E_trans_cell_2 <- simulate_trans_expression(A_trans = A_cell_2$A_trans, phi_gene = phi_gene,
+                                                E_cis = E_cis,A_cis_trans = A_cell_2$A_cis_trans, type = 'ACC')
+    
+    # Determine omega if not provided
+    if (is.null(omega)) {
+        omega <- runif(1, 0.2, 0.8)
+    }
+    
+    # Weighted sum
+    E_trans <- omega * E_trans_cell_1 + (1 - omega) * E_trans_cell_2
+    
+    return(list(E_trans = E_trans, E_trans_cell_1 = E_trans_cell_1, E_trans_cell_2 = E_trans_cell_2))
 }
 
 
